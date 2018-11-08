e50f866300830e157a30b53569fe6eae753dff87
thebarratt <jeff@barratt.ca>
2018-11-08 15:08:37 -0500

    Update update-html.blade.php

--- resources/views/emails/components/update-html.blade.php
+++ resources/views/emails/components/update-html.blade.php
@@ -12,7 +12,7 @@
                 <w:anchorlock/>
                 <center style="color:#ffffff;font-family:sans-serif;font-size:15px;">{!! trans('cachet.subscriber.email.manage') !!}</center>
                 </v:roundrect><![endif]-->
-                <a href="{{ $manage_link }}" class="button button--green">{!! trans('cachet.subscriber.email.manage') !!}</a>
+                <a href="{{ $manage_link }}" class="button button--green">{!! trans('cachet.subscriber.email.manage') !!} TESTING!!! </a>
             </div>
         </td>
     </tr>
