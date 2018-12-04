0e939383b0d42a0e82456edfe17eb7c7f89260fb
thebarratt <jeff@barratt.ca>
2018-12-04 15:31:12 -0500

    Update update-html.blade.php

--- resources/views/emails/components/update-html.blade.php
+++ resources/views/emails/components/update-html.blade.php
@@ -7,12 +7,12 @@
     <tr>
         <td align="center">
             <div>
-                <!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="/" style="height:45px;v-text-anchor:middle;width:200px;" arcsize="7%" stroke="f" fill="t">
+                <!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://rescachet/" style="height:45px;v-text-anchor:middle;width:200px;" arcsize="7%" stroke="f" fill="t">
                 <v:fill type="tile" color="#22BC66" />
                 <w:anchorlock/>
                 <center style="color:#ffffff;font-family:sans-serif;font-size:15px;">{{ trans('cachet.link') }}</center>
                 </v:roundrect><![endif]-->
-                <a href="/" class="button button--green">{{ trans('cachet.link') }}</a>
+                <a href="http://rescachet/" class="button button--green">{{ trans('cachet.link') }}</a>
             </div>
         </td>
     </tr>
